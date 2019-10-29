784b9a02d419246a377b2c12936c1a31ec518f7d
swidzinskiMS <dswidzinski@makingsense.com>
2019-10-29 20:06:07 -0300

    db

--- boot.php
+++ boot.php
@@ -1,9 +1,3 @@
-<?php
-    $db_connection = pg_connect("host=ec2-174-129-253-47.compute-1.amazonaws.com dbname=dd9lraajhtiljc user=xeuhldijwddzdw password=3309ad02e81eebea23fdb7fbd8ce55cd78a9f6db4d6ea705678ba9c070192dcc");
-    $result = pg_query($db_connection, "SELECT 'lastname'");
-    
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,7 +32,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#"><?php $result ?></a>
+            <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Services</a>

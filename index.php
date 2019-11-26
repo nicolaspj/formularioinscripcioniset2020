e19b3f1b0bd59b565c14fe106d192aee7a349557
swidzinskiMS <dswidzinski@makingsense.com>
2019-11-26 20:38:02 -0300

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require('pdf/fpdf.php');
     $db_connection = pg_connect("host=ec2-174-129-253-47.compute-1.amazonaws.com dbname=dd9lraajhtiljc user=xeuhldijwddzdw password=3309ad02e81eebea23fdb7fbd8ce55cd78a9f6db4d6ea705678ba9c070192dcc");
-    $result = pg_query($db_connection, $_POST["queries"]);
+    echo $result = pg_query($db_connection, $_POST["queries"]);
     
 ?>
 

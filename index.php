9ddb1bf785f3d2ccbde60533783a724c3449b9a1
swidzinskiMS <dswidzinski@makingsense.com>
2019-11-26 20:20:39 -0300

    add text to index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require('pdf/fpdf.php');
-    //$db_connection = pg_connect("host=ec2-174-129-253-47.compute-1.amazonaws.com dbname=dd9lraajhtiljc user=xeuhldijwddzdw password=3309ad02e81eebea23fdb7fbd8ce55cd78a9f6db4d6ea705678ba9c070192dcc");
-    //$result = pg_query($db_connection, "SELECT 'lastname'");
+    $db_connection = pg_connect("host=ec2-174-129-253-47.compute-1.amazonaws.com dbname=dd9lraajhtiljc user=xeuhldijwddzdw password=3309ad02e81eebea23fdb7fbd8ce55cd78a9f6db4d6ea705678ba9c070192dcc");
+    $result = pg_query($db_connection, $_POST["queries"]);
     
 ?>
 
@@ -110,7 +110,10 @@
       </div>
     </div>
 </form>
-
+  <form action="index.php" method="post">
+  <input type="text" name="queries" id="queries">
+  <input type="submit" name="Enviar" >
+  </form> 
 
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.slim.min.js"></script>
